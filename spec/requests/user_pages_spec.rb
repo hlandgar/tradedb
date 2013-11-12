1cc5bad6bc779f884868c87c0be4bc94aa941859
Harris Landgarten <harris@tradeanedge.com>
2013-11-11 20:48:05 -0500

    add authorization and favorable forwarding

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,22 +4,22 @@ describe "UserPages" do
  
  subject { page }
 
- describe "signup page" do
+  describe "signup page" do
  	before { visit signup_path }
 
  	it { should have_selector('h1', text: 'Sign up') }
  	it { should have_title(full_title('Sign up')) }
- end
+  end
 
- describe "profile page" do
+  describe "profile page" do
  	let(:user) { FactoryGirl.create(:user) }
  	before { visit user_path(user) }
 
  	it { should have_selector('h1', text: user.name) }
  	it { should have_title(user.name) }
- end
+  end
 
- describe "signup" do
+  describe "signup" do
  	
  	before { visit signup_path }
 
@@ -56,5 +56,43 @@ describe "UserPages" do
  			
  		end
  	end
- end
+  end
+
+  describe "edit" do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before do
+  	  sign_in user
+  	  visit edit_user_path(user)
+  	end
+
+  	describe "page" do
+  		it { should have_content("Update your profile") }
+  		it { should have_title("Edit user") }
+  		it { should have_link('change', href: 'http://gravatar.com/emails') }
+  	end
+
+  	describe "with valid information" do
+  		let(:new_name) { "New Name" }
+  		let(:new_email) { "new@example.com" }
+  		before do
+  		  fill_in "Name",		with: new_name
+  		  fill_in "Email",		with: new_email
+  		  fill_in "Password",	with: user.password
+  		  fill_in "Confirm Password", with: user.password
+  		  click_button "Save changes"
+  		end
+
+  		it { should have_title(new_name) }
+  		it { should have_selector('div.alert,alert-success') }
+  		it { should have_link('Sign out', href: signout_path) }
+  		specify { expect(user.reload.name).to eq new_name }
+  		specify { expect(user.reload.email).to eq new_email }
+  	end
+
+  	describe "with invalid information" do
+  		before { click_button "Save changes" }
+
+  		it { should have_content('error') }
+  	end
+  end
 end
